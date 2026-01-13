92e77c91aaa72124c7e6331f0460c58c595d01b5
Tyran169 <reallyvuwm@gmail.com>
2026-01-13 12:39:11 +0700

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,5 +1,15 @@
 import { Metadata } from 'next';
 
+import favicon from '@/assets/images/favicon.ico';
+import logoSvg from '@/assets/images/logo.svg';
+import icon16x16 from '@/assets/images/icon-16x16.png';
+import icon32x32 from '@/assets/images/icon-32x32.png';
+import icon48x48 from '@/assets/images/icon-48x48.png';
+import icon64x64 from '@/assets/images/icon-64x64.png';
+import icon96x96 from '@/assets/images/icon-96x96.png';
+import appleIcon from '@/assets/images/apple-icon.png';
+import ogImage from '@/assets/images/og-image.png';
+
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
 
 export const metadata: Metadata = {
@@ -9,49 +19,49 @@ export const metadata: Metadata = {
   icons: [
     {
       rel: 'icon',
-      url: '/images/favicon.ico',
+      url: favicon.src,
       sizes: '48x48',
       type: 'image/x-icon'
     },
     {
       rel: 'icon',
-      url: '/images/logo.svg',
+      url: logoSvg.src,
       sizes: 'any',
       type: 'image/svg+xml'
     },
     {
       rel: 'icon',
-      url: '/images/icon-16x16.png',
+      url: icon16x16.src,
       sizes: '16x16',
       type: 'image/png'
     },
     {
       rel: 'icon',
-      url: '/images/icon-32x32.png',
+      url: icon32x32.src,
       sizes: '32x32',
       type: 'image/png'
     },
     {
       rel: 'icon',
-      url: '/images/icon-48x48.png',
+      url: icon48x48.src,
       sizes: '48x48',
       type: 'image/png'
     },
     {
       rel: 'icon',
-      url: '/images/icon-64x64.png',
+      url: icon64x64.src,
       sizes: '64x64',
       type: 'image/png'
     },
     {
       rel: 'icon',
-      url: '/images/icon-96x96.png',
+      url: icon96x96.src,
       sizes: '96x96',
       type: 'image/png'
     },
     {
       rel: 'apple-touch-icon',
-      url: '/images/apple-icon.png',
+      url: appleIcon.src,
       sizes: '180x180',
       type: 'image/png'
     }
@@ -61,9 +71,20 @@ export const metadata: Metadata = {
     capable: true
   },
   openGraph: {
+    title: 'Grerth',
+    description: 'Just for fun!',
     siteName: 'Grerth',
     url: '/',
-    type: 'website'
+    type: 'website',
+    images: [
+      {
+        url: ogImage.src,
+        width: 1200,
+        height: 630,
+        alt: 'Site Preview Image',
+        type: 'image/png'
+      }
+    ]
   },
   manifest: '/site.webmanifest'
 };
